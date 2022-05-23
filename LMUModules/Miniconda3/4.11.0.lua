5ecaba486bac1d46d858f962674ec86486319409
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2022-05-23 15:23:16 +0300

    new miniconda version

---
+++ LMUModules/Miniconda3/4.11.0.lua
@@ -0,0 +1,11 @@
+help ([[For detailed instructions, go to:
+ https://...
+]])
+
+whatis("Version: 4.11.0")
+whatis("Keywords: Miniconda3 4.11.0")
+whatis("URL: ")
+whatis("Description: This is Miniconda3!")
+
+--source-sh("bash", "/proj/hajaalin/Miniconda3/4.11.0/etc/profile.d/conda.sh")
+prepend_path( "PATH", "/proj/hajaalin/Miniconda3/4.11.0/bin")
