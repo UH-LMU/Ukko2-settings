fd712b6a3f1195cc6aca1e3fff23155d9e80c06f
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2022-10-21 16:59:58 +0300

    create Miniconda3 module in /proj/group/lmu

---
+++ slurm_modules/Miniconda3/4.12.0_py38.lua
@@ -0,0 +1,11 @@
+help ([[For detailed instructions, go to:
+ https://...
+]])
+
+whatis("Version: 4.12.0_py38")
+whatis("Keywords: Miniconda3 4.12.0_py38")
+whatis("URL: ")
+whatis("Description: This is Miniconda3!")
+
+--source-sh("bash", "/proj/group/lmu/software/Miniconda3/py38_4.12.0/etc/profile.d/conda.sh")
+prepend_path( "PATH", "/proj/group/lmu/software/Miniconda3/py38_4.12.0/bin")
